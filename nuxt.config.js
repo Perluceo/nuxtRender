9eb25c252c07ce6c3bec8e04ae8898569e6ed4ed
user <fullstackdevsmu@gmail.com>
2023-11-03 00:53:38 -0700

    nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
 export default {
-  server: 'static'
+  server: 'static',
+  link: [
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+    { rel: "stylesheet", href: "/css/font-awesome/css/all.css" },
+    { rel: "stylesheet", href: "/css/default.css" }
+  ],
+  css: [],
 }
 
